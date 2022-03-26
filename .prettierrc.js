14377cbb5b56667f3548a0a6960f84a348845581
ambar <ambar.lee@gmail.com>
2022-03-26 10:16:05 +0800

    chore: use react18

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('prettier').Options}
+ * @see https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  bracketSpacing: false,
+  semi: false,
+  singleQuote: true,
+}
