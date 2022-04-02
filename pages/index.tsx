c3d58a9c07e8e60cd88429a1958e6ec0d68d64a1
ambar <ambar.lee@gmail.com>
2022-04-02 09:44:40 +0800

    update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,8 @@ const Home: NextPage = () => {
           name="description"
           content="方言字,汉语方言用字规范,现代汉语方言大词典"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </Head>
 
       <FangyanziTable />
