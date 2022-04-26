ecbc3cb7c55076e8b506e888e8d57021ff3b4ab3
ambar <ambar.lee@gmail.com>
2022-04-26 17:40:21 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 import type {AppProps} from 'next/app'
 import '../styles/globals.css'
 
+// 不指定 fallback 时 iOS 偶有 bug（有 query string 访问主体显示为粗体）
+const system = `-apple-system,BlinkMacSystemFont,system-ui`
 /** https://zh.m.wikipedia.org/wiki/宋体 */
-const song = `'Songti SC','Noto Serif CJK SC','Source Han Serif SC','Source Han Serif CN',STSong,NSimSun,SimSun,PMingLiU,MingLiU,serif`
+const song = `'Songti SC','Noto Serif CJK SC','Source Han Serif SC','Source Han Serif CN',STSong,NSimSun,SimSun,PMingLiU,MingLiU`
 /** https://chakra-ui.com/docs/styled-system/theming/theme */
 const customTheme = extendTheme({
   config: {
@@ -11,11 +13,11 @@ const customTheme = extendTheme({
     // useSystemColorMode: true,
   },
   fonts: {
-    body: song,
-    heading: song,
+    body: `${song},${system},serif`,
+    heading: `${song},${system},serif`,
     song: song,
-    ipa: `Doulos SIL,Arial,${song}`,
-    cjkExt: `BabelStoneHan,Hana,${song}`,
+    ipa: `Doulos SIL,Arial,${song},${system},serif`,
+    cjkExt: `BabelStoneHan,Hana,${song},${system},serif`,
   },
   styles: {
     global: {
