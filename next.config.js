5845416a177a7661004f142c88e07db89121854d
ambar <ambar.lee@gmail.com>
2022-11-01 11:21:24 +0800

    build: use next 13

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   // 让 router 工作正常
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || undefined,
+  ...(process.env.NEXT_PUBLIC_BASE_PATH && {
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  }),
   // 让部署工作正常
   // https://nextjs.org/docs/deployment#static-only
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || undefined,
+  ...(process.env.NEXT_PUBLIC_ASSET_PREFIX && {
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+  }),
 }
 
 module.exports = nextConfig
